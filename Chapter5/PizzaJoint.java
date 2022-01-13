3e1210e10fa643a70f1bff11cc833af36809c855
AloyFusion <48078029+AloyFusion@users.noreply.github.com>
2022-01-13 13:30:31 -0600

    ss

--- Chapter5/PizzaJoint.java
+++ Chapter5/PizzaJoint.java
@@ -1,7 +1,7 @@
 /**
  * a simple cash register for a pizza joint
  *
- * @author Alex Wang Alex R
+ * @author Alex Wang, Alex R
  * @version 1/13/22
  */
 
@@ -10,14 +10,57 @@ import java.util.*;
 public class PizzaJoint {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int choice = 0;
+        Register reg = new Register(123);
+        int choice, pass;
 
-        while () {
-            System.out.println("1) unlock");
-            System.out.println("2) exit");
-
+        while (true) {
+            System.out.println();
+            System.out.println("1) Start order");
+            System.out.println("2) Statistics");
+            System.out.println("3) unlock");
+            System.out.println("4) lock");
+            System.out.println("5) exit");
             choice = input.nextInt();
-
+            
+            //Start order
+            if (choice == 1) {
+                if (!(reg.locked())) {
+                
+                }
+                else
+                    System.out.println("Register is locked");
+            }
+            //Statistics
+            if (choice == 2) {
+                if (!(reg.locked())) {
+                    System.out.println();
+                    System.out.println("Total sales: " + Register.getTotalSales());
+                    System.out.println("Total number of orders: " + Register.getNumOfOrders());
+                    System.out.println("Total number of pizzas: " + Register.getPizzasOrdered());
+                    System.out.println("Total number of toppings: " + Register.getNumOfToppings());
+                    System.out.println("Enter any integer to continue");
+                    input.nextLine();
+                }
+                else
+                    System.out.println("Register is locked");
+            }
+            //unlock
+            if (choice == 3) {
+                System.out.println("Enter your password: ");
+                pass = input.nextInt();
+                reg.unlock(pass);
+            }
+            //lock
+            if (choice == 4) {
+                System.out.println("Enter your password: ");
+                pass = input.nextInt();
+                reg.lock(pass);
+            }
+            //exit
+            if (choice == 5)
+                break;
+            else
+                System.out.println("error");
         }
 
     }
--- Chapter5/Register.java
+++ Chapter5/Register.java
@@ -7,25 +7,56 @@
  * @version 1/12/22
  */
 public class Register implements lockable {
-    private static pizzasOrdered = 0, numOfOrders = 0;
+    private static int numberOfToppings, pizzasOrdered, numOfOrders, totalSales;
+    private int password;
+    private boolean isLocked;
+    
 
-    public register () {
-        private boolean isLocked
-        private String password = "asd";
+    public Register (int pass) {
+        password = pass;
+        isLocked = true;
+        numberOfToppings = 0;
+        pizzasOrdered = 0;
+        numOfOrders = 0;
+        totalSales = 0;
+    }
+    
+    public static int getNumOfToppings () {
+        return numberOfToppings;
+    }
+    
+    public static int getPizzasOrdered () {
+        return pizzasOrdered;
+    }
+    
+    public static int getNumOfOrders () {
+        return numOfOrders;
+    }
+    
+    public static int getTotalSales () {
+        return totalSales;
     }
 
     public void setkey (int pass) {
         this.password = pass;
     }
 
-    public void lock ( int locker) {
-        if (locker == this.password)
+    public void lock (int locker) {
+        if (locker == this.password) {
             this.isLocked = true;
+            System.out.println("locked");
+        }
+        else
+            System.out.println("Error");
     }
 
     public void unlock(int unlocker) {
-        if (unlocker == this.password)
+        if (unlocker == this.password) {
             this.isLocked = false;
+            System.out.println("unlocked");
+        }
+        else
+        System.out.println("Error");
     }
 
     public boolean locked () {
